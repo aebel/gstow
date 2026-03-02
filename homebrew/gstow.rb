30fd01f3d598bc141ed625980b08fdb36413dde0
Alexander Ebel <info@ebel-systems.de>
2026-03-02 15:43:46 +0100

    Update Homebrew formula for v0.1.1

--- homebrew/gstow.rb
+++ homebrew/gstow.rb
@@ -1,28 +1,28 @@
 class Gstow < Formula
   desc "Dotfile manager inspired by GNU Stow with per-package configuration"
   homepage "https://github.com/aebel/gstow"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_intel do
       url "https://github.com/aebel/gstow/releases/download/v#{version}/gstow_#{version}_darwin_amd64.tar.gz"
-      sha256 "b4208911316b3194f59e593cc16ede63822aabd41887981eeaef84f8f7160bad"
+      sha256 "80adbb443694263921eee98621a9bb7861441745c751110c21c1ba3beeb4808c"
     end
     on_arm do
       url "https://github.com/aebel/gstow/releases/download/v#{version}/gstow_#{version}_darwin_arm64.tar.gz"
-      sha256 "1041cc4c35a86ddc77759a5c054ed31ce479c44f5f40dc86fa9948ab1b96da5f"
+      sha256 "5f6c1307190cd0d760c3539b8ee0c40f2ef78c34eaec298439e2b4b180ebb520"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/aebel/gstow/releases/download/v#{version}/gstow_#{version}_linux_amd64.tar.gz"
-      sha256 "929d234e2058309f267b3b19506885e4b93f5d058b8cf6bade1ccb22b3f35f33"
+      sha256 "fe04236df321b158843fa64369a527e3078234eecb3b49c7f874da0bcbd8b133"
     end
     on_arm do
       url "https://github.com/aebel/gstow/releases/download/v#{version}/gstow_#{version}_linux_arm64.tar.gz"
-      sha256 "237cbb6b5c6bfff5aa257c855e1ef770f35f8b70ec371e863fbb1d9ec0304972"
+      sha256 "6eeafdd15df08911296f157dd154eb27aca50e107470fc9ccc295ecc53cf57cb"
     end
   end
 
